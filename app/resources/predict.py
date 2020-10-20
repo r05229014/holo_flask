25a4ea8d2aa703e393a4b24752b857689b1b582a
r05229014 <r05229014@gmail.com>
2020-10-20 02:58:40 +0000

    update to transfer gcp

--- app/resources/predict.py
+++ app/resources/predict.py
@@ -2,6 +2,7 @@ import os
 import re
 import json
 import numpy as np
+import requests
 from flask import Flask, request, jsonify
 from flask_restful import Resource
 from flask import current_app as app
@@ -11,37 +12,40 @@ from .utils import (
 
 
 class Predict(Resource):
+    def __init__(self):
+
+        self.ws, self.pos, self.ner = load_models(app.config['MODEL_PATH'])
+        self.need_pos = ['Na', 'Nb', 'Nc']
+        
+
     def get(self):
-        pass
+        return 'testhaha'
 
     def post(self):
         if request.is_json:
             contents = request.get_json()
-        if contents is not None:
-            print('Loading model...')
-            ws, pos, ner = load_models(app.config['MODEL_PATH'])
-            need_pos = ['Na', 'Nb', 'Nc']
         
         # start to processing
         sentence_list = []
         for data in contents:
             sentence_list.append(data['content'])
         
-        word_sentence_list = ws(sentence_list,
+        word_sentence_list = self.ws(sentence_list,
                                 sentence_segmentation = True,  # To consider delimiters
                                 segment_delimiter_set = {",", "，", "。", ":", "?", "!", ";",
                                                         "(", ")", "（", "）",
                                                         ".", "%", "、", "·", "《", "》",
                                                         "「", "」", "：", "#", ""})
-        pos_sentence_list = pos(word_sentence_list)
-        entity_sentence_list = ner(word_sentence_list, pos_sentence_list)
+        pos_sentence_list = self.pos(word_sentence_list)
+        entity_sentence_list = self.ner(word_sentence_list, pos_sentence_list)
 
         for idx, (sentence, word, pos, entity) in enumerate(zip(sentence_list, word_sentence_list, pos_sentence_list, entity_sentence_list)):
-            pos_output = processing_pos(sentence, word, pos, need_pos)
+            pos_output = processing_pos(sentence, word, pos, self.need_pos)
             ner_output = processing_ner(sentence, entity)
             final_output = dict(pos_output, **ner_output)
 
             contents[idx]['POS'] = pos_output
             contents[idx]['NER'] = ner_output
             contents[idx]['Final'] = final_output
+
         return jsonify(contents)
--- run.py
+++ run.py
@@ -12,4 +12,4 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True, port=5000) 
+    app.run(host='0.0.0.0', port=8080, debug=True) 
